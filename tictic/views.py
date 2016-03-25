31f5f47d1067d6be51712b0dc34da86d35a03b2a
sean <imprint75@gmail.com>
2016-03-25 18:13:54 +0200

    caching the game state

--- tictic/views.py
+++ tictic/views.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 
 from django.http import JsonResponse
 from django.views.generic import View
+from django.core.cache import cache
 
 logger = logging.getLogger(__name__)
 
@@ -10,4 +11,6 @@ logger = logging.getLogger(__name__)
 class StartGameView(View):
     def get(self, request, *args, **kwargs):
         gid = uuid4()
+        board = []
+        cache.set(gid, board, timeout=None)
         return JsonResponse({'gid': gid})
