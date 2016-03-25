478acb8715dda0a1ff4caca254978b57adb89916
sean <imprint75@gmail.com>
2016-03-25 18:21:51 +0200

    initiate new game in start

--- tictic/views.py
+++ tictic/views.py
@@ -5,12 +5,15 @@ from django.http import JsonResponse
 from django.views.generic import View
 from django.core.cache import cache
 
+from libs.tictactoe import Tic
+
 logger = logging.getLogger(__name__)
 
 
 class StartGameView(View):
     def get(self, request, *args, **kwargs):
+        tic = Tic()
         gid = uuid4()
-        board = []
+        board = tic.show()
         cache.set(gid, board, timeout=None)
-        return JsonResponse({'gid': gid})
+        return JsonResponse({'gid': gid, 'board': board})
