c6375ccfc06d7ff3e03ca4b0c15809dd06bcc6b2
sean <imprint75@gmail.com>
2016-03-28 14:55:51 +0300

    updating names for tests

--- tictic/urls.py
+++ tictic/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from tictic.views import StartGameView, GameMoveView
 
 urlpatterns = [
-    url(r'^start/?$', StartGameView.as_view(), name='index'),
-    url(r'^move?$', GameMoveView.as_view(), name='move'),
+    url(r'^start/?$', StartGameView.as_view(), name='tictic.index'),
+    url(r'^move?$', GameMoveView.as_view(), name='tictic.move'),
 ]
