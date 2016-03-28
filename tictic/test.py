83060baa378e8c26c3c4b1aa7d702b0518bfcd1e
sean <imprint75@gmail.com>
2016-03-28 15:00:18 +0300

    use django TestCase and test basic view stuff

--- tictic/test.py
+++ tictic/test.py
@@ -1,9 +1,27 @@
-import unittest
+import json
+
+from django.core.urlresolvers import reverse
+from django.test import TestCase
 
 from libs.gameplay import make_move, post_move, flatten_board
 
 
-class TestGamePlay(unittest.TestCase):
+class TestGamePlay(TestCase):
+
+    def setUp(self):
+        response = self.client.get(reverse('tictic.index'))
+        self.assertEqual(response.status_code, 200)
+        response_content = json.loads(str(response.content, encoding='utf8'))
+        self.assertIn('gid', response_content.keys())
+        self.gid = response_content['gid']
+
+    def test_post_to_start(self):
+        response = self.client.post(reverse('tictic.index'))
+        self.assertEqual(response.status_code, 405)
+
+    def test_get_to_move(self):
+        response = self.client.get(reverse('tictic.move'))
+        self.assertEqual(response.status_code, 405)
 
     def test_flatten_board(self):
         self.assertEqual(flatten_board([]), [])
